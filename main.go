bec9cfdd805e6dbc26687bf63721c300dfe8fcf0
jmclellan <jvl.mclellan@gmail.com>
2018-02-01 21:25:21 -0500

    added todo list, changed return tyoe of store feed

--- main.go
+++ main.go
@@ -5,32 +5,41 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
+// TODO
+// 1. shift database to be an actual database
+// 2. implement interface for multiple rss urls
+// 3. implement a loop to check feeds every XX minutes
+//
+//
+
 var database = make(map[string]*gofeed.Item)
 var testURL = "http://www.wuxiaworld.com/feed/"
 
 var feedparser = gofeed.NewParser()
 
+// generates the items key idetifier
 func uniqueIdentifier(feedItem *gofeed.Item) string {
 	return feedItem.Link
 }
 
-func storeFeed(url string) string {
+// stored all elements into the feed using their
+func storeFeed(url string) bool {
 	var feed, _ = feedparser.ParseURL(url)
 
 	var i = 0 // counter
 	for i = 0; i < len(feed.Items); i++ {
 		database[uniqueIdentifier(feed.Items[i])] = feed.Items[i]
 	}
-	return feed.Items[0].Title
+	return true
 }
 
 func main() {
 
 	print(len(database))
-	var result = storeFeed(testURL)
+	//var result = storeFeed(testURL)
 	print(len(database))
 	print("hey its working.\n")
-	print(result)
+	//print(result)
 	//getFeed(testURL)
 
 }
