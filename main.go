25c8f50048846d7db64c9849a8950a697186a523
jmclellan <jvl.mclellan@gmail.com>
2018-02-01 21:09:34 -0500

    changed database to be an array and re implemented storeFeed to use it

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	//"fmt"
 	"github.com/mmcdole/gofeed"
-	//	"fmt"
 )
 
-var database = make(map[string]gofeed.Item)
+var database = make(map[string]*gofeed.Item)
 var testURL = "http://www.wuxiaworld.com/feed/"
 
 var feedparser = gofeed.NewParser()
 
-func storeFeed(url string, destination *[]gofeed.Item) string {
+func uniqueIdentifier(feedItem *gofeed.Item) string {
+	return feedItem.Link
+}
+
+func storeFeed(url string) string {
 	var feed, _ = feedparser.ParseURL(url)
 
 	var i = 0 // counter
 	for i = 0; i < len(feed.Items); i++ {
-		destination.push(feed.Items[i])
+		database[uniqueIdentifier(feed.Items[i])] = feed.Items[i]
 	}
 	return feed.Items[0].Title
 }
@@ -23,7 +27,7 @@ func storeFeed(url string, destination *[]gofeed.Item) string {
 func main() {
 
 	print(len(database))
-	var result = storeFeed(testURL, database)
+	var result = storeFeed(testURL)
 	print(len(database))
 	print("hey its working.\n")
 	print(result)
