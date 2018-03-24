3f43f07d92d77bd2ac32d20f8cd1e1613b03afcd
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2018-03-24 15:25:23 -0400

    Starting to pull out handler functionalty

--- server.go
+++ server.go
@@ -9,6 +9,9 @@ import (
 	"net/http"
 )
 
+type appHandler func(http.ResponseWriter, *http.Request) (int, error)
+
+// Request Structs
 type testStruct struct {
 	Test string
 }
@@ -18,34 +21,50 @@ type feedEntry struct {
 	Category string
 }
 
-//addFeedSource
+// Handler Functions
+
+// Generalized handler functions
+func apiHandler(rq http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var t testStruct
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	log.Println(t.Test)
+}
+
+// How can I pass the DB object to the handler function?
+func addFeedHandler(rw http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var t feedEntry
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	log.Println(t)
+	//addFeedSource(t.FeedURL, t.Category, db)
+	fmt.Fprintf(rw, "Success! The feed has been added\n")
+}
+
+func withLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Before")
+		next.ServeHTTP(w, r)
+		log.Println("After")
+	})
+}
+
+// Server
 func startServer(db *sql.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	http.HandleFunc("/test", func(rw http.ResponseWriter, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var t testStruct
-		err := decoder.Decode(&t)
-		if err != nil {
-			panic(err)
-		}
-		defer req.Body.Close()
-		log.Println(t.Test)
-	})
-	http.HandleFunc("/add-feed", func(rw http.ResponseWriter, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var t feedEntry
-		err := decoder.Decode(&t)
-		if err != nil {
-			panic(err)
-		}
-		defer req.Body.Close()
-		log.Println(t)
-		addFeedSource(t.FeedURL, t.Category, db)
-		fmt.Fprintf(rw, "Success! The feed has been added\n")
-	})
+	http.HandleFunc("/test", apiHandler)
+	http.HandleFunc("/add-feed", addFeedHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
